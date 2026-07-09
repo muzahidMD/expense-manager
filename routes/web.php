0e9ab7011fe6d2ec2e974c533870926a071eaf7d
muzahidulzavisoft <muzahidul.zavisoft@gmail.com>
2026-07-09 17:46:08 +0600

    controller up

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', [CategoryController::class, 'index']);
+Route::post('/create', [CategoryController::class, 'create']);
